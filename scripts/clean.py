e3f174931740cffba7c462ec9ffc895a8650a76b
Marina <140115877+mhmarina@users.noreply.github.com>
2026-02-21 15:46:12 -0700

    init

---
+++ scripts/clean.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+outfile = "adult.data.clean.csv"
+infile = "adult.data.csv"
+
+df = pd.read_csv(infile, header=None, index_col=False)
+
+init = df.count()
+df.drop_duplicates(inplace=True)
+df.dropna(inplace=True)
+
+mask = df.eq(' ?').any(axis=1)
+df = df[~mask]
+
+final = df.count()
+print(f"dropped {init - final} rows")
+
+df.to_csv(outfile, header=None, index=False)
